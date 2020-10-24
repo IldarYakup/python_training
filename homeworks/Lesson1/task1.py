abcb5a9de9c4997b6b19d457610971809c5d3308
IldarYakup <IldarYakup@gmail.com>
2020-10-24 21:20:44 +0300

    Lesson1 second task

---
+++ homeworks/Lesson1/task1.py
@@ -0,0 +1,6 @@
+first_variable = "Привет"
+second_variable = "пользователь"
+print( first_variable, second_variable )
+user_name = input("Введите ваше имя: ")
+user_age = input("Введите сколько вам полных лет: ")
+print("Здравствуй " + user_name +" " + user_age + " лет")
