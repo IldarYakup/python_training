bfcbb55b150cfcd329f4e96ea13d936642dfb778
IldarYakup <IldarYakup@gmail.com>
2020-10-25 11:40:39 +0300

    Lesson1 third task complete

--- homeworks/Lesson1/task3.py
+++ homeworks/Lesson1/task3.py
@@ -1,3 +1,3 @@
 user_number = input("Задайте число n, которое вставится в формулу n + nn + nnn: ")
 formula = int(str(user_number)) + int(str(user_number + user_number)) + int(str(user_number + user_number + user_number))
-print(formula)
+print(str(user_number) + "+" + str(user_number + user_number) + "+" + str(user_number + user_number + user_number) + "=" + str(formula))
