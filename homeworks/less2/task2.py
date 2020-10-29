21834ebe81c31e43afc95ea1733aa18aad8fdda4
IldarYakup <IldarYakup@gmail.com>
2020-10-29 16:04:02 +0300

    Task2 complete

--- homeworks/less2/task2.py
+++ homeworks/less2/task2.py
@@ -1,4 +1,9 @@
 my_list = []
+my_list_2 = []
+my_list_3 = []
+user_add = input("Введите элемент, который нужно добавить в список: ")
+my_list.append(user_add)
+n = 0
 while True:
     user_ans = input("Добавить элемент в список? \n Y - Да\n N - Нет: ")
     if user_ans.upper() == "Y":
@@ -8,13 +13,18 @@ while True:
         break
     else:
         print("Ошибка в ответе")
-print(my_list)
-
-
-
-
-my_list2 = ['1', '2', '3', '4']
-tuple(my_list2)
-for el in my_list2:
-    my_list2.append(el * 2)
-print(my_list2)
+while len(my_list) > 1:
+    index_1 = my_list[0]
+    index_2 = my_list[1]
+    my_list_2.insert(n + 1, index_1)
+    my_list_2.insert(n, index_2)
+    my_list.pop(0)
+    my_list.pop(0)
+    my_list_3.extend(my_list_2)
+    my_list_2.pop(0)
+    my_list_2.pop(0)
+    if len(my_list) == 1:
+        my_list_3.extend(my_list)
+    elif len(my_list) < 1:
+        break
+print(my_list_3)
