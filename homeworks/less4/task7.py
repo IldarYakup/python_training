db49d7d7db3dfd53fe73c0b2cbadccb314523c71
IldarYakup <IldarYakup@gmail.com>
2020-11-08 11:56:18 +0300

    Homework complete

--- homeworks/less4/task7.py
+++ homeworks/less4/task7.py
@@ -4,18 +4,14 @@
 Функция отвечает за получение факториала числа, а в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
 Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 """
-def fict(n):
-    factorial = 1
-    for i in range(2, n + 1):
-        factorial *= i
-        yield
+def fact(n):
+    prev_num = 1
+    result = 1
+    while n:
+        yield result
+        prev_num += 1
+        n -= 1
+        result *= prev_num
 
-while True:
-    n = input("Введите число для нахождения факториала: ")
-    try:
-        n = int(n)
-    except ValueError:
-        print("Введенное значение не является числом")
-        continue
-
-print(fict(n))
+for idx, itm in enumerate(fact(15), 1):
+    print(idx, itm)
