56e5a767115b489a3df3e96b99ce16094982ab80
IldarYakup <IldarYakup@gmail.com>
2020-11-11 16:21:42 +0300

    Task №2 complete

---
+++ homeworks/less5/task2.py
@@ -0,0 +1,11 @@
+"""
+2. Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк,
+количества слов в каждой строке.
+"""
+
+my_f = open("ForTask2.txt", "r")
+content = my_f.readlines()
+for idx, word in enumerate(content, 1):
+    word = word.count(" ")
+    print(f'В строке под №{idx}, {word} слов')
+my_f.close()
