97fbe72aebfcc56d41219b1c155c84bea8be3a24
IldarYakup <IldarYakup@gmail.com>
2020-11-13 09:01:22 +0300

    Task №4 in complete

--- homeworks/less5/Task4.py
+++ homeworks/less5/Task4.py
@@ -8,22 +8,22 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+list= []
 my_f = open("ForTask4.txt", "r", encoding="UTF-8")
-read_file = my_f.read()
-list = read_file.split()
-print(list)
+read_file = my_f.readline()
+for line in read_file:
+    if len(read_file.split()) == 0:
+        break
+    list.append(read_file.split())
+    read_file = my_f.readline()
+my_f.close()
 my_dict = {"One" : "Один", "Two" : "Два", "Three" : "Три", "Four" : "Четыре"}
-for i, e in enumerate(list):
-    if e in my_dict:
-        list[i] = my_dict[e]
-i = 0
-while True:
-    print(list[i:[i+2]])
-    i += 2
-"""output = " "
-output_new = output.join(list)
-print(output_new)"""
-"""for a, b, c in output_new:
-    str = a + b + c
-    print(str)
-"""
+for el in list:
+    for i, e in enumerate(el):
+        if e in my_dict:
+            el[i] = my_dict[e]
+output_new = ""
+with open('task4.txt', 'w', encoding='utf-8') as new:
+    for idx, el in enumerate(list):
+        output_new += (" ".join(list[idx]) + "\n")
+    new.write(output_new)
